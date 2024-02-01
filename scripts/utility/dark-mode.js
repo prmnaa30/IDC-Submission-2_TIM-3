45b43eb624079f3abf5517044b9ed89feaff52ab
prmnaa30 <73104057+prmnaa30@users.noreply.github.com>
2024-02-01 13:00:46 +0800

    weather data icons now follows the dark mode rules

--- scripts/utility/dark-mode.js
+++ scripts/utility/dark-mode.js
@@ -3,6 +3,10 @@ const modeToggler = document.getElementsByClassName("mode_toggler")[0];
 const logoLight = document.getElementsByClassName("logo-light")[0];
 const logoDark = document.getElementsByClassName("logo-dark")[0];
 
+let weatherImage = document.getElementById("weather-image");
+let weatherIcon = document.querySelectorAll(".left-part img");
+let sunRiseSet = document.querySelectorAll(".sunrise-sunset img");
+
 const theme = document.documentElement; //* mengambil elemen/tag "html"
 
 /** dark mode <==> light mode */ 
@@ -11,16 +15,26 @@ function lightMode() {
     // ubah logo menjadi light
     logoLight.classList.toggle("hidden");
     logoDark.classList.toggle("hidden");
+    // ubah weather icon menjadi dark
+    weatherImage.style.filter = "invert(0)";
+    weatherIcon.forEach(image => { image.style.filter = "invert(0)"; });
+    sunRiseSet.forEach(image => { image.style.filter = "invert(0)"; });
 }
 function darkMode() {
     theme.setAttribute("data-theme", "dark");
     // ubah logo menjadi dark
     logoLight.classList.toggle("hidden");
     logoDark.classList.toggle("hidden");
+    // ubah weather icon menjadi light
+    weatherImage.style.filter = "invert(0.95)";
+    weatherIcon.forEach(image => { image.style.filter = "invert(0.95)"; });
+    sunRiseSet.forEach(image => { image.style.filter = "invert(0.95)"; });
 }
 
-modeToggler.addEventListener("click", () => {
-    const currentTheme = localStorage.getItem("Theme");
+document.addEventListener("DOMContentLoaded", () => {
+    window.localStorage.setItem("Theme", "Light");
+
+    modeToggler.addEventListener("click", () => {
     const modeImage = document.getElementById("mode-image");
 
     // jika DOM 'theme' memiliki atribut data-theme dengan value "light"
@@ -30,7 +44,7 @@ modeToggler.addEventListener("click", () => {
         // ubah tombol sun menjadi moon
         modeImage.setAttribute("src", "assets/svg/dark-mode.svg");
         // 
-        localStorage.setItem("Theme", "dark");
+        localStorage.setItem("Theme", "Dark");
 
     } else {
         // FALSE, ubah value dari atribut data-theme menjadi light
@@ -38,12 +52,17 @@ modeToggler.addEventListener("click", () => {
         // ubah tombol moon menjadi sun
         modeImage.setAttribute("src", "assets/svg/light-mode.svg");
         // 
-        localStorage.setItem("Theme", "light");
+        localStorage.setItem("Theme", "Light");
     }
 });
+})
 
+/** function checkTheme
+ * Untuk mengecek theme saat ini (light / dark).
+ * Berguna agar saat berpindah halaman, mode dark/light mengikuti halaman sebelumnya (index <=> weather_CRUD).
+ */
 function checkTheme() {
-    if (localStorage.getItem("Theme") === "light") {
+    if (localStorage.getItem("Theme") === "Light") {
         lightMode();
     } else {
         darkMode();
