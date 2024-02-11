6c7b009b69dcc5627d8db7d567766a35d371ff46
prmnaa30 <73104057+prmnaa30@users.noreply.github.com>
2024-02-11 23:00:36 +0800

    optimized darkmode code

--- scripts/utility/dark-mode.js
+++ scripts/utility/dark-mode.js
@@ -1,70 +1,54 @@
 /** deklarasi DOM */
-const modeToggler = document.getElementsByClassName("mode_toggler")[0];
-const logoLight = document.getElementsByClassName("logo-light")[0];
-const logoDark = document.getElementsByClassName("logo-dark")[0];
+const modeToggler = document.getElementById("mode_toggler");
 
 let weatherImage = document.getElementById("weather-image");
-let weatherIcon = document.querySelectorAll(".left-part img");
-let sunRiseSet = document.querySelectorAll(".sunrise-sunset img");
+let weatherIcons = document.querySelectorAll(".left-part img");
+let sunRise_Set = document.querySelectorAll(".sunrise-sunset img");
 
 const theme = document.documentElement; //* mengambil elemen/tag "html"
 
-/** dark mode <==> light mode */ 
-function lightMode() {
-    theme.setAttribute("data-theme", "light");
-    // ubah logo menjadi light
-    logoLight.classList.toggle("hidden");
-    logoDark.classList.toggle("hidden");
-    // ubah weather icon menjadi dark
-    weatherImage.style.filter = "invert(0)";
-    weatherIcon.forEach(image => { image.style.filter = "invert(0)"; });
-    sunRiseSet.forEach(image => { image.style.filter = "invert(0)"; });
-}
-function darkMode() {
-    theme.setAttribute("data-theme", "dark");
-    // ubah logo menjadi dark
-    logoLight.classList.toggle("hidden");
-    logoDark.classList.toggle("hidden");
-    // ubah weather icon menjadi light
-    weatherImage.style.filter = "invert(0.95)";
-    weatherIcon.forEach(image => { image.style.filter = "invert(0.95)"; });
-    sunRiseSet.forEach(image => { image.style.filter = "invert(0.95)"; });
-}
+let darkMode = false;
 
-document.addEventListener("DOMContentLoaded", () => {
-    window.localStorage.setItem("Theme", "Light");
-
-    modeToggler.addEventListener("click", () => {
-    const modeImage = document.getElementById("mode-image");
-
-    // jika DOM 'theme' memiliki atribut data-theme dengan value "light"
-    if (theme.getAttribute("data-theme") === "light") {
-        // TRUE, ubah value dari atribut data-theme menjadi dark
-        darkMode();
-        // ubah tombol sun menjadi moon
-        modeImage.setAttribute("src", "assets/svg/dark-mode.svg");
-        // 
-        localStorage.setItem("Theme", "Dark");
-
-    } else {
-        // FALSE, ubah value dari atribut data-theme menjadi light
-        lightMode();
-        // ubah tombol moon menjadi sun
-        modeImage.setAttribute("src", "assets/svg/light-mode.svg");
-        // 
-        localStorage.setItem("Theme", "Light");
+// Periksa key 'darkMode' pada localStorage (jika tersedia)
+window.addEventListener("load", () => {
+    // jika pada localStorage terdapat key/item darkMode
+    if (localStorage.getItem('darkMode')) {
+        darkMode = JSON.parse(localStorage.getItem('darkMode'));
+        toggleDarkMode();
     }
 });
-})
 
-/** function checkTheme
- * Untuk mengecek theme saat ini (light / dark).
- * Berguna agar saat berpindah halaman, mode dark/light mengikuti halaman sebelumnya (index <=> weather_CRUD).
- */
-function checkTheme() {
-    if (localStorage.getItem("Theme") === "Light") {
-        lightMode();
-    } else {
-        darkMode();
-    }
+// Fungsi saat tombol darkMode toggle diklik
+modeToggler.addEventListener("click", () => {
+    darkMode = !darkMode;
+    toggleDarkMode();
+    localStorage.setItem('darkMode', JSON.stringify(darkMode));
+});
+
+function toggleDarkMode() {
+    const darkFilter = darkMode ? 'invert(0.95)' : 'invert(0)';
+    weatherIcons.forEach(image => { image.style.filter = darkFilter; });
+    sunRise_Set.forEach(image => { image.style.filter = darkFilter; });
+    if (weatherImage) weatherImage.style.filter = darkFilter;
+
+    const logoImage = darkMode ? '../assets/svg/logo_dark.svg' : '../assets/svg/logo_light.svg';
+    document.getElementById("logo-image").src = logoImage;
+    const togglerImage = darkMode ? '../assets/svg/dark-mode.svg' : '../assets/svg/light-mode.svg';
+    document.getElementById("mode-image").src = togglerImage;
+
+    const currentTheme = darkMode ? 'dark' : 'light';
+    theme.setAttribute("data-theme", currentTheme);
 }
+
+/** NOTES */
+
+/** const abcdef = condition ? ifTrue : ifFalse
+ * adalah shorthand dari fungsi if else, bentuk aslinya:
+ * 
+ * const abcdef = "";
+ * if (condition) {
+ *     abcdef = ifTrue;
+ * } else {
+ *     abcdef = ifFalse;
+ * }
+ */
